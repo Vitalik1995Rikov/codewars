efd4a3a5e5b45541c040b145680db278d4b6ecca
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-04-13 22:14:48 +0300

    bubblesort

--- ex_25/script.js
+++ ex_25/script.js
@@ -1,4 +1,14 @@
-function selectionSort(arr) {}
+function bubleSort(array) {
+  for (let j = 0; j < array.length - 1; j++) {
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] < array[i + 1]) {
+        let n = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = n;
+      }
+    }
+  }
+  return array;
+}
 
-let array = [5, 3, 6, 2, 10];
-console.log(selectionSort(array));
+console.log(bubleSort([1, 2, 3, 4, 5, 6, 7, 8]));
