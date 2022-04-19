da256b2db073130c6893fa5bc64c8cefc3f24c6a
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-04-19 23:24:28 +0300

    first iteraction

--- ex_30/script.js
+++ ex_30/script.js
@@ -1,3 +1,13 @@
-function findStr(str, needle) {}
+function findStr(str, needle) {
+  let arrOne = str.split('');
+  let arrTwo = needle.split('');
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] == arrTwo[0]) {
+      if (arrOne[i + 1] == arrTwo[1]) {
+        return true;
+      }
+    }
+  }
+}
 
-console.log(findStr());
+console.log(findStr('qwerty', 'er'));
