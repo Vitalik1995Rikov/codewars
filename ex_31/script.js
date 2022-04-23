1b32b7e032e1f4cbd9dc9e099e4934419fc15e83
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-04-23 16:51:57 +0300

    first iteraction

--- ex_31/script.js
+++ ex_31/script.js
@@ -1,3 +1,17 @@
-function findDoubl(arr) {}
+function findDoubl(arr) {
+  arr.sort((a, b) => a - b);
+  let count = arr.length;
+  for (let j = 0; j < arr.length; j++) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == arr[i + 1]) {
+        arr.splice(i + 1, 1);
+      }
+    }
+  }
+  for (let i = 0; i < count - arr.length; i++) {
+    arr.push('_');
+  }
+  return arr;
+}
 
-console.log(findDoubl());
+console.log(findDoubl([1, 2, 4, 3, 4, 2, 4, 2]));
