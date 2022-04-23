eaa321d9cdd81f941fd0312f53897fb3f45d7b88
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-04-23 18:44:52 +0300

    second solution

--- ex_32/script.js
+++ ex_32/script.js
@@ -1,13 +1,12 @@
 function removeElement(arr, num) {
-  let newArr = [];
   let j = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] !== num) {
-      newArr[j] = arr[i];
+      arr[j] = arr[i];
       j++;
     }
   }
-  console.log(newArr);
+  return j;
 }
 
-removeElement([1, 2, 3, 4, 2, 5, 2, 1, 5], 2);
+console.log(removeElement([1, 2, 3, 4, 2, 5, 2, 1, 5], 2));
